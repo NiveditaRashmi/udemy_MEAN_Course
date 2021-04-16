925a3db2c9a0b00991f74232c7cbdb1d32d50287
Nivrash <nrashmi2009@gmail.com>
2021-04-16 22:59:28 +0530

    posts-service added

---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Post } from './post.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+  private posts: Post[]
+  constructor() { }
+}
